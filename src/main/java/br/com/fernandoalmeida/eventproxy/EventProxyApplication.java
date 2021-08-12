cf9a46f955007cfe2376edc3f3891d04ad3d959e
Fernando Almeida <fernando.c.almeida@gmail.com>
2021-08-12 18:51:05 -0300

    openapi support

--- src/main/java/br/com/fernandoalmeida/eventproxy/EventProxyApplication.java
+++ src/main/java/br/com/fernandoalmeida/eventproxy/EventProxyApplication.java
@@ -3,11 +3,16 @@ package br.com.fernandoalmeida.eventproxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Eventproxy API", description = "EventProxy api allows any external service to be called, with an optional delay", contact = @Contact(name = "Fernando Costa de Almeida", email = "fernando.c.almeida@gmail.com")))
 public class EventProxyApplication
 {
-	public static void main(String[] args)
-	{
-		SpringApplication.run(EventProxyApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EventProxyApplication.class, args);
+    }
 }
