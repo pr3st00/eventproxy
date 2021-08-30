66115cbb66956979d001cf2e6a0930c33b99bb57
Fernando Almeida <fernando.c.almeida@gmail.com>
2021-08-30 12:59:22 -0300

    Initial security config

--- src/test/java/br/com/fernandoalmeida/eventproxy/EventRestControllerTest.java
+++ src/test/java/br/com/fernandoalmeida/eventproxy/EventRestControllerTest.java
@@ -1,13 +1,15 @@
 package br.com.fernandoalmeida.eventproxy;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,7 +25,7 @@ import br.com.fernandoalmeida.eventproxy.caller.Request;
 import br.com.fernandoalmeida.eventproxy.config.ApplicationConfig;
 import br.com.fernandoalmeida.eventproxy.web.EventRestController;
 
-@WebMvcTest(controllers = EventRestController.class)
+@WebMvcTest(controllers = EventRestController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @AutoConfigureMockMvc
 class EventRestControllerTest
 {
