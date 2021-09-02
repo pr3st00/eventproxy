e28a8166df8635090f16b756483c7409dc8afc0e
Fernando Almeida <fernando.c.almeida@gmail.com>
2021-09-02 12:08:42 -0300

    docker script

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Runs the eventproxy container
@@ -8,19 +8,19 @@ CONTAINER_NAME=eventproxy
 
 # Defaults. Values from .env file can overwrite those
 DOCKER_USER=pr3st00
-SERVER.PORT=8081
-SPRING.USERNAME=admin
-SPRING.PASSWORD=password
+SERVER_PORT=8081
+SPRING_USERNAME=admin
+SPRING_PASSWORD=password
 
 if [[ -f .env ]]; then
 	echo "Loading variables from .env file"
 	. ./.env
-]]
+fi
 
-docker run --restart always -d -p ${SERVER.PORT}:${SERVER.PORT} \
-  -e SERVER.PORT=${SERVER.PORT} \
-  -e SPRING.USERNAME=${SPRING.USERNAME} \
-  -e SPRING.PASSWORD=${SPRING.PASSWORD} \
+docker run --restart always -d -p ${SERVER_PORT}:${SERVER_PORT} \
+  -e SERVER.PORT=${SERVER_PORT} \
+  -e SPRING.USERNAME=${SPRING_USERNAME} \
+  -e SPRING.PASSWORD=${SPRING_PASSWORD} \
   --name $CONTAINER_NAME ${DOCKER_USER}/${CONTAINER_NAME}
 
 # EOF
